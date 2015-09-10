27287887b5433cfc07ba5d9c3c61550de4247c27
Gabriel Deschildre <gabriel.deschildre@egdweb.fr>
2015-09-10 15:21:14 +0200

    BugFix:old route name remained

--- Resources/views/admin/feeds/partials/_scripts.blade.php
+++ Resources/views/admin/feeds/partials/_scripts.blade.php
@@ -1,10 +1,6 @@
 <script>
     $( document ).ready(function() {
-        $(document).keypressAction({
-            actions: [
-                { key: 'b', route: "<?= route('admin.ffnet.matchdays.index') ?>" }
-            ]
-        });
+     
 
 
 
